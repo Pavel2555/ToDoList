59c70500179568bfa11117d5a68b2b69516e2dbb
Pavel2555 <86742948+Pavel2555@users.noreply.github.com>
2023-06-08 23:07:04 +0300

    Add files via upload

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Павел Бескоровайный on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
